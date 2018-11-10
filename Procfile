5636a188f23aa790252002095f09539c26b02d88
Paul Orlan Dumdum <dumdumpaulorlan06@gmail.com>
2018-11-10 13:53:36 +0800

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/spring-rest-service-0.0.1-SNAPSHOT.jar --server.port=80
---
+++ src/main/java/io/dumerz/springrestservice/DumerzSpringRestServiceApplication.java
@@ -0,0 +1,12 @@
+package io.dumerz.springrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumerzSpringRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumerzSpringRestServiceApplication.class, args);
+	}
+}
